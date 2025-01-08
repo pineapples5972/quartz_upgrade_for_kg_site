ad2bb5f14f6c3ec75e4cb8e7d1d995b190cfcbb1
Ｎａｇｋｕｍａｒ です <99494692+karpurgauram@users.noreply.github.com>
2025-01-08 20:34:11 +0530

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "karpurgauram",
+    pageTitle: "Ｎａｇｋｕｍａｒ です",
     enableSPA: true,
     enablePopovers: true,
     locale: "en-US",
