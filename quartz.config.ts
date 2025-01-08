3d71089eb362438f1a943501d2dcbb2448445368
Ｎａｇｋｕｍａｒ です <99494692+karpurgauram@users.noreply.github.com>
2025-01-08 20:35:11 +0530

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -12,7 +12,7 @@ const config: QuartzConfig = {
     enableSPA: true,
     enablePopovers: true,
     locale: "en-US",
-    baseUrl: "pratyabhijna.exozy.me",
+    baseUrl: "karpurgauram.xyz",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
